3138b59945c11a6deab22006b37c91d0aac9f672
AlexPlot <plotnikov1988@gmail.com>
2026-01-05 19:41:45 +0100

    fix: remove nested git and add project files

---
+++ project18/src/assets/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "360",
+    id: 1,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "234",
+    id: 2,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "134",
+    id: 3,
+  },
+  {
+    image: "https://i.imgur.com/PReV51j.png",
+    name: "Мужские Кроссовки Nike Air Zoom Pegasus",
+    price: "175",
+    id: 4,
+  },
+];
